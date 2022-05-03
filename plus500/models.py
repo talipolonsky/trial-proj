a0c3d75aa6a202e975eb790cd58b726436f2694d
Tali Polonsky <taliplons@walla.com>
2022-05-03 16:49:02 +0300

    what

--- plus500/models.py
+++ plus500/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class Plus500(models.Model):
-    url_from = models.CharField(max_length=250)
+    url_from = models.CharField(max_length=250, blank=True)
+    url_to = models.CharField(max_length=250, blank=True)
     ahrefs_rank = models.IntegerField()
     domain_rating = models.IntegerField()
+    title = models.CharField(max_length=900, blank=True)
+
 
     def __str__(self):
         return self.url_from
--- plus500/views.py
+++ plus500/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from plus500.models import Plus500
 import requests
+from django.views.generic import CreateView
+from .forms import HomeForm
+
 # Create your views here.
+
 def get_data(request):
     all_links = {}
-    url ='https://apiv2.ahrefs.com?from=backlinks&target=ahrefs.com&mode=domain&order_by=ahrefs_rank%3Adesc&select=url_from,ahrefs_rank,domain_rating&output=json&token=082c4afc97f7348b730e5fc0b861a2ebd9ce522a'
+    Plus500.objects.all().delete()
+    url ='https://apiv2.ahrefs.com?from=backlinks&target=ahrefs.com&mode=subdomains&order_by=ahrefs_rank%3Adesc&select=url_from,ahrefs_rank,domain_rating,url_to,title&output=json&token=082c4afc97f7348b730e5fc0b861a2ebd9ce522a'
     try:
         response = requests.get(url)
         data = response.json()
@@ -14,11 +19,19 @@ def get_data(request):
     for i in links:
         link_data = Plus500(
             url_from = i['url_from'],
+            url_to = i['url_to'],
             ahrefs_rank = i['ahrefs_rank'],
-            domain_rating = i['domain_rating']
+            domain_rating = i['domain_rating'],
+            title = i['title']
         )
         link_data.save()
         all_links = Plus500.objects.all()
-
-    return render (request, 'refpages/refpage.html', { "all_links":
+    return render(request, 'refpages/refpage.html', { "all_links":
     all_links} )
+
+def get_home_form(request):
+    if request.method == "post":
+        home_form = HomeForm(request.POST)
+    else:
+        home_form = HomeForm()
+    return render(request, 'refpages/refpage.html', {'home_form':home_form})
