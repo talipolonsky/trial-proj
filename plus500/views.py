84d9eb5e0361559b0bf9f2573cea549a910c6968
el1yarden <yarden@yarden>
2022-05-03 16:30:34 +0300

    Koral&Yarden 3.5 16:30 (create home_form & table)

--- plus500/views.py
+++ plus500/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from plus500.models import Plus500
 import requests
+from django.views.generic import CreateView
+from .forms import HomeForm
+
 # Create your views here.
 def get_data(request):
     all_links = {}
@@ -22,3 +25,10 @@ def get_data(request):
 
     return render (request, 'refpages/refpage.html', { "all_links":
     all_links} )
+
+def get_home_form(request):
+    if request.method == "post":
+        home_form = HomeForm(request.POST)
+    else:
+        home_form = HomeForm()
+    return render(request, 'refpages/refpage.html', {'home_form':home_form})
