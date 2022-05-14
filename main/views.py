06ad7ec65e4377ebfb2e08c57682d8a37bf06cf7
Tali Polonsky <taliplons@walla.com>
2022-05-14 12:30:01 +0300

    d

--- main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model, login, \
-                                logout, authenticate
-
-
-def home(request):
-    return render(request, 'main/home.html')
-# Create your views here.
