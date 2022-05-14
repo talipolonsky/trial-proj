c4debf65b56d41362f478d57b26e30cf882864d9
ofir0avni-technion <ofir.avni@campus.technion.ac.il>
2022-05-14 12:26:16 +0300

    chgannmm2

--- plus500/models.py
+++ plus500/models.py
@@ -12,11 +12,11 @@ class Plus500(models.Model):
     def __str__(self):
         return self.url_from
 
-class Settings_table(models.Model):
-    id = models.IntegerField()
-    domain_rating = models.IntegerField()
-    title = models.CharField(max_length=900, blank=True)
-    #category =  models.CharField(max_length=50, blank=True)
+#class Settings_table(models.Model):
+#    id = models.IntegerField()
+#    domain_rating = models.IntegerField()
+#    title = models.CharField(max_length=900, blank=True)
+#    #category =  models.CharField(max_length=50, blank=True)
 
-    def __str__(self):
-        return self.url_from
+#    def __str__(self):
+#        return self.url_from
