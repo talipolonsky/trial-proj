f6824e6dfe915bf84c8af47b178074210ab7d282
ofir0avni-technion <ofir.avni@campus.technion.ac.il>
2022-06-04 18:36:15 +0300

    ofir changes #2 4-6

--- plus500/models.py
+++ plus500/models.py
@@ -41,7 +41,6 @@ class Settings_table(models.Model):
     #sorting priorities of the fields:
     domain_rating_priority = models.IntegerField(null=True)
     domain_traffic_priority = models.IntegerField(null=True)
-    traffic_trend_priority = models.IntegerField(null=True)
     referringDomains_backlinks_ratio_priority = models.IntegerField(null=True)
     #cometitors bolean fields (True if selected, else False):
     avatrae = models.BooleanField(default=False)
@@ -50,7 +49,7 @@ class Settings_table(models.Model):
     IG = models.BooleanField(default=False)
     CMC_markets = models.BooleanField(default=False)
     #the email template
-    email_template = models.CharField(max_length=10000, null=True)
+    email_template = models.CharField(max_length=100000, null=True)
     #last number of links:
     links_num = models.IntegerField(null=True)
 
--- plus500/views.py
+++ plus500/views.py
@@ -51,11 +51,40 @@ def home(request):
         setting_object.links_num = 50
 
     num_of_links= setting_object.links_num
-    selected_links = selected_links.filter(domain_rating__gt=setting_object.domain_rating)[:num_of_links]
-    #selected_links = selected_links.order_by('-pub_date', 'headline')[:num_of_links]
-    #he result above will be ordered by pub_date descending, then by headline ascending. The negative sign in front of "-pub_date" indicates descending order. Ascending order is implied.
-    context.update({'selected_links': selected_links, 'num_of_links': num_of_links})
 
+    # filter on unselected competitors:
+    all_competitors = {"avatrae": setting_object.avatrae,
+         "robinhood": setting_object.robinhood,"etoro": setting_object.etoro,
+         "IG": setting_object.IG,"CMC_markets": setting_object.CMC_markets}
+
+    unselected_competitors = []
+    for competitor, bool_competitor in all_competitors.items():
+        if not bool_competitor:
+            unselected_competitors.append(competitor)
+    for unselected_competitor in unselected_competitors:
+        selected_links = selected_links.exclude(competitor=unselected_competitor)
+
+    # filter on minimum in each metric:
+    selected_links = selected_links.filter(domain_rating__gt=setting_object.domain_rating)
+
+    #sorting by selected priorities:
+    #explanation of -: selected_links = selected_links.order_by('-pub_date', 'headline')[:num_of_links]
+    #he result above will be ordered by pub_date descending, then by headline ascending. The negative sign in front of "-pub_date" indicates descending order. Ascending order is implied.
+    priority_dict = {setting_object.domain_rating_priority: '-domain_rating',
+                    setting_object.domain_traffic_priority: '-domain_traffic',
+                    setting_object.referringDomains_backlinks_ratio_priority: '-referringDomains_backlinks_ratio'}
+    #for key in sorted(priority_dict):
+    try:
+        first_order = priority_dict[0]
+        second_order = priority_dict[1]
+        third_order = priority_dict[2]
+        selected_links = selected_links.order_by(first_order, second_order, third_order)
+    except:
+        print('unable to prioritiez the matrics')
+    #selected_links = selected_links.order_by('-domain_rating_priority', '-domain_traffic_priority')[:num_of_links]
+
+    selected_links = selected_links[:num_of_links]
+    context.update({'selected_links': selected_links, 'num_of_links': num_of_links})
     return render(request, 'plus500/home.html', context)
 
 @login_required
@@ -73,11 +102,13 @@ def settings(request):
     #send flags of exceptions:
     # form = StyleSettings
     # context = {'form': form}
-    context = {'domain_rating_exception': False, 'domain_traffic_exception': False,
-     'RB_ratio_exception': False, 'none_exception': False}
+    context = {'domain_rating_exception': False, 'domain_traffic_exception': False, 'RB_ratio_exception': False,
+    'priority_range_exception': False, 'email_template_exception': False, 'none_exception': False}
+
     setting_object = Settings_table.objects.get(id__exact=1)
     #updating the fields of the object with id=1:
     if request.method == 'POST':
+        #sorting metrics:
         if request.POST.get('DR'):
             try:
                 setting_object.domain_rating = int(request.POST.get('DR'))
@@ -94,10 +125,56 @@ def settings(request):
             except:
                 context.update({'RB_ratio_exception': True})
 
+        #sorting prioritities:
+        try:
+            dr = int(request.POST.get('DR_priority'))
+            dt = int(request.POST.get('DT_priority'))
+            br = int(request.POST.get('RB_ratio_priority'))
+            #check if the priority includes all: 0,1,2
+            priority_range = (0,1,2)
+            for i in priority_range:
+                if i not in (dr, dt, br):
+                    context.update({'priority_range_exception': True})
+                    break
+            if not context['priority_range_exception']:
+                setting_object.domain_rating_priority = dr
+                setting_object.domain_traffic_priority = dt
+                setting_object.referringDomains_backlinks_ratio_priority = br
+        except:
+            context.update({'priority_range_exception': True})
+
+        #competitor companies:
+        if request.POST.get('avatrae'):
+            setting_object.avatrae = True
+        else:
+            setting_object.avatrae = False
+        if request.POST.get('robinhood'):
+            setting_object.robinhood = True
+        else:
+            setting_object.robinhood = False
+        if request.POST.get('etoro'):
+            setting_object.etoro = True
+        else:
+            setting_object.etoro = False
+        if request.POST.get('IG'):
+            setting_object.IG = True
+        else:
+            setting_object.IG = False
+        if request.POST.get('CMC_markets'):
+            setting_object.CMC_markets = True
+        else:
+            setting_object.CMC_markets = False
+
+        #Email Template:
+        try:
+            setting_object.email_template = request.POST.get('email_template')
+        except:
+            context.update({'email_template_exception': True})
+
         setting_object.save()
 
         #for alert of Success:
-        none_exception = not context['domain_rating_exception'] and not context['domain_traffic_exception'] and not context['RB_ratio_exception']
+        none_exception = not context['domain_rating_exception'] and not context['domain_traffic_exception'] and not context['RB_ratio_exception'] and not context['email_template_exception'] and not context['priority_range_exception']
         if none_exception:
                 context.update({'none_exception': True})
 
