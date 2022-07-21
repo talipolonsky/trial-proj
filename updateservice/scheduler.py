31d1bef0eb6c9341f944827abe99a25f5fe2915d
ofir0avni-technion <ofir.avni@campus.technion.ac.il>
2022-07-21 11:52:08 +0300

    all competitors

--- updateservice/scheduler.py
+++ updateservice/scheduler.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from updateservice import update
 
 def start():
-    #scheduler = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 10})
-    #scheduler.add_job(update.get_data,'interval', hours=15)
+    #scheduler = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 20})
+    #scheduler.add_job(update.get_data,'interval', minutes=50)
     #scheduler.start()
     return 0
