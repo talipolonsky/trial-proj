991c758a3824da7a6403f816a655062fd54f2e89
ofir0avni-technion <ofir.avni@campus.technion.ac.il>
2022-07-31 13:10:23 +0300

    LAST CHANGE OFIR 31-7

--- plus500/forms.py
+++ plus500/forms.py
@@ -7,4 +7,4 @@ from plus500.models import Plus500, Settings_table
 class ContactForm(forms.Form):
     to_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True, initial=Settings_table.objects.get(id__exact=1).email_template)
